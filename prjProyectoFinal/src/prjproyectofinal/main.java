4275095ace7e6e755f50e8cba4be4b5bcbe71ec1
Aronnmp <113871644+Aronnmp@users.noreply.github.com>
2022-10-07 12:06:54 -0600

    Update main.java
    vea ver

--- prjProyectoFinal/src/prjproyectofinal/main.java
+++ prjProyectoFinal/src/prjproyectofinal/main.java
@@ -8,6 +8,14 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int cont = 0;
+        do
+        {
+            System.out.println(cont);;
+            cont++;
+        }
+        while(cont<=10);
+        
         // TODO code application logic here
     }
     
