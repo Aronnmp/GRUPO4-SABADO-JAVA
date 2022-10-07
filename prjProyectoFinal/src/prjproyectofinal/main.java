b03f9e1bd5272fd5176bcc3e75484a4c828c397a
Aronnmp <113871644+Aronnmp@users.noreply.github.com>
2022-10-07 12:15:00 -0600

    Update main.java

--- prjProyectoFinal/src/prjproyectofinal/main.java
+++ prjProyectoFinal/src/prjproyectofinal/main.java
@@ -11,6 +11,10 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int cont = 0;
+        do
+        {
+            System.out.println
         // TODO code application logic here
     }
     
