cf65a770277adccba583e258421e0f2fc3d85ec2
Juanpa <113861584+13Juanpaxd@users.noreply.github.com>
2022-11-12 11:41:01 -0600

    Add files via upload
    proyecto verdadero

---
+++ Proyectfinal/src/main/Main.java
@@ -0,0 +1,14 @@
+
+package main;
+
+import interfazG.grafica;
+ 
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+       grafica sudoku = new grafica();
+       sudoku.setVisible(true); 
+    }
+}
---
+++ Proyectfinal/src/matrices/tiempo_juego.java
@@ -0,0 +1,20 @@
+
+package matrices;
+
+
+public class tiempo_juego {
+    //aquí se pondrán lo que son: cronómetro del tiempo que dure.
+    //si rompe algún record de juego
+    //guardar datos sobre el tiempo que duró.
+    
+    public class cronometro{
+    
+    }
+    
+    public class records{
+    
+    }
+    public class reinicio_record{
+    
+    }
+}
